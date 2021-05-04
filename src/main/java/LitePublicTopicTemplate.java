6fef2e45661e20403bd3e6e45d3e519cc0709df5
Tianzi Cai <tianzi@google.com>
2021-05-03 17:04:15 -0700

    use PubsubLiteIO released with Beam

--- src/main/java/LitePublicTopicTemplate.java
+++ src/main/java/LitePublicTopicTemplate.java
@@ -1,11 +1,11 @@
 import com.google.cloud.pubsublite.Message;
 import com.google.cloud.pubsublite.TopicPath;
-import com.google.cloud.pubsublite.beam.PublisherOptions;
-import com.google.cloud.pubsublite.beam.PubsubLiteIO;
 import com.google.cloud.pubsublite.proto.PubSubMessage;
 import com.google.protobuf.ByteString;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
+import org.apache.beam.sdk.io.gcp.pubsublite.PublisherOptions;
+import org.apache.beam.sdk.io.gcp.pubsublite.PubsubLiteIO;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
@@ -13,8 +13,8 @@ import org.apache.beam.sdk.options.StreamingOptions;
 import org.apache.beam.sdk.options.Validation.Required;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.values.TypeDescriptor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 
 public class LitePublicTopicTemplate {
 
@@ -27,7 +27,7 @@ public class LitePublicTopicTemplate {
     void setPubsubLiteTopic(String value);
   }
 
-  private static final Logger LOG = LoggerFactory.getLogger(LitePublicTopicTemplate.class);
+  // private static final Logger LOG = LoggerFactory.getLogger(LitePublicTopicTemplate.class);
 
   public static void main(String[] args) {
 
@@ -55,7 +55,7 @@ public class LitePublicTopicTemplate {
                     (String m) -> {
                       Message message =
                           Message.builder().setData(ByteString.copyFromUtf8(m)).build();
-                      LOG.info("Created: " + m);
+                      // LOG.info("Created: " + m);
                       return message.toProto();
                     }))
         .apply("Write to Pub/Sub Lite", PubsubLiteIO.write(publisherOptions));
